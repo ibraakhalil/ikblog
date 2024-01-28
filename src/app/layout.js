886b54a59e04a768ced96e3bf05d759fc09087e8
ibrahim khalil <ibkhalil5425@gmail.com>
2024-01-28 23:26:39 +0600

    test1

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ props, children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body className={poppins.className}>
         <SessionProvider>
           <BlogProvider>
